9dd381f1a583a8187ec623e16a8930dcca72e497
andreaa21 <andrea.zollini@proton.me>
2022-10-17 17:18:24 +0200

    milestone 2

---
+++ MILESTONE 2/js/script.js
@@ -0,0 +1,19 @@
+const images = [
+    01.jpg,
+    02.jpg,
+    03.jpg,
+    04.jpg,
+    05.jpg
+];
+
+let tagImg = '';
+const slider = document.querySelector('.slider');
+
+for(i = 0; i < images.length; i++){
+    tagImg += `
+        <img class="item" src="img/${images[i]}" alt="${images[i]}">
+    `;
+}
+
+slider.innerHTML += tagImg;
+
