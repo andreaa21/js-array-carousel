930dd1f98b3340aeb5538ffda0021ee38b570eed
andreaa21 <andrea.zollini@proton.me>
2022-10-17 19:15:31 +0200

    add if condition to click event

--- MILESTONE 3/js/script.js
+++ MILESTONE 3/js/script.js
@@ -18,15 +18,24 @@ for(let i = 0; i < images.length; i++){
 slider.innerHTML += tagImg;
 
 let imageCounter = 0; 
+
 const items = document.getElementsByClassName('item');
 items[imageCounter].classList.add('active');
 
+const down = document.querySelector('.down');
+const up = document.querySelector('.up');
 
 down.addEventListener('click', function(){
     items[imageCounter].classList.remove('active');
     imageCounter++;
     items[imageCounter].classList.add('active');
     console.log(imageCounter);
+
+    if(imageCounter === images.lenght -1){
+        imageCounter = 0;
+        items[0].classList.add('active');
+    }
+
 });
 up.addEventListener('click', function(){
     items[imageCounter].classList.remove('active');
@@ -34,3 +43,6 @@ up.addEventListener('click', function(){
     items[imageCounter].classList.add('active');
     console.log(imageCounter);
 });
+
+
+
